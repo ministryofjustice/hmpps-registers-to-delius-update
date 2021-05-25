9522de6aeea238f4b0e3ac37b1140f5d8c6a6798
karenmillermoj <karen.miller@digital.justice.gov.uk>
2021-05-25 09:30:36 +0100

    DT-1644 Update Probation system when court update event received

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsregisterstodeliusupdate/services/CourtRegisterUpdateService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsregisterstodeliusupdate/services/CourtRegisterUpdateService.kt
@@ -4,14 +4,22 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsregisterstodeliusupdate.model.CourtUpdate
+import uk.gov.justice.digital.hmpps.hmppsregisterstodeliusupdate.services.ProbationService.CourtForProbationSystem
 
 @Service
-class CourtRegisterUpdateService {
+class CourtRegisterUpdateService(
+  private val courtRegisterService: CourtRegisterService,
+  private val probationService: ProbationService
+) {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
-  fun updateCourtRegister(court: CourtUpdate) {
+  fun updateCourtDetails(court: CourtUpdate) {
     log.info("About to update court $court")
+    courtRegisterService.getCourtInfoFromRegister(court.courtId)?.run {
+      val dataPayload = CourtForProbationSystem(this)
+      probationService.updateCourt(dataPayload)
+    }
   }
 }
